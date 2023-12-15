49622c04ce30d48c1ddf0cade18d3e0aebffede5
Serhii Vidernikov <sv54@mscloud.ua.es>
2023-12-15 12:04:34 +0100

    correcciones

--- Ionic/iVideoclub/src/app/videoclub/videoclub.page.ts
+++ Ionic/iVideoclub/src/app/videoclub/videoclub.page.ts
@@ -4,11 +4,13 @@ import { PeliculasService } from '../peliculas.service';
 
 
 @Component({
-  selector: 'app-videoclub',
-  templateUrl: './videoclub.page.html',
-  styleUrls: ['./videoclub.page.scss'],
+    selector: 'app-videoclub',
+    templateUrl: './videoclub.page.html',
+    styleUrls: ['./videoclub.page.scss'],
 })
 export class VideoclubPage implements OnInit {
+
+    modoLista = true
 
     listaPeliculas: any[]
     constructor(private router: Router, private peliculasServicio: PeliculasService) {
@@ -19,31 +21,35 @@ export class VideoclubPage implements OnInit {
         this.router.navigate(['/detalle', id]);
     }
 
-
-  ngOnInit(){
-    console.log('pagina videoclub cargada');
-  }
-
-  ionViewDidEnter() {
-    console.log('ionViewDidEnter videoclub page');
-  }
-
-  ionViewWillEnter(){
-    console.log('ionViewWillEnter videoclub page');
-  }
+    cambiarVista(){
+        this.modoLista = !this.modoLista
+    }
 
 
-  ionViewWillLeave(){
-    console.log('ionViewWillLeave videoclub page');
+    ngOnInit() {
+        console.log('pagina videoclub cargada');
+    }
 
-  }
+    ionViewDidEnter() {
+        console.log('ionViewDidEnter videoclub page');
+    }
 
-  ionViewDidLeave(){
-    console.log('ionViewDidLeave videoclub page');
+    ionViewWillEnter() {
+        console.log('ionViewWillEnter videoclub page');
+    }
 
-  }
 
-  ngOnDestroy(){
-    console.log('ngOnDestroy videoclub page');
-  }
+    ionViewWillLeave() {
+        console.log('ionViewWillLeave videoclub page');
+
+    }
+
+    ionViewDidLeave() {
+        console.log('ionViewDidLeave videoclub page');
+
+    }
+
+    ngOnDestroy() {
+        console.log('ngOnDestroy videoclub page');
+    }
 }
