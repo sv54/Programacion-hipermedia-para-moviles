9b319edd30b74a43cb491e8f69172f6af70f7041
Serhii Vidernikov <sv54@mscloud.ua.es>
2024-01-15 18:42:05 +0100

    terminado el ejercicio 2

--- Laravel/videoclub/resources/views/catalog/create.blade.php
+++ Laravel/videoclub/resources/views/catalog/create.blade.php
@@ -1,33 +1,48 @@
 @extends('layouts.master')
 
 @section('content')
-    <form action="" method="POST">
-        @csrf
-        <div class="form-group">
-            <label for="titulo">Título:</label>
-            <input type="text" class="form-control" id="titulo" name="titulo" required>
+    <div class="row" style="margin-top:40px">
+        <div class="offset-md-3 col-md-6">
+            <div class="card">
+                <div class="card-header text-center">
+                    Añadir película
+                </div>
+                <div class="card-body" style="padding:30px">
+                    <form action="" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <label for="title">Título</label>
+                            <input type="text" name="title" id="title" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="año">Año</label>
+                            <input type="text" class="form-control" id="año" name="año" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="director">Director</label>
+                            <input type="text" class="form-control" id="director" name="director" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="poster">Poster</label>
+                            <input type="text" class="form-control" id="poster" name="poster" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="synopsis">Resumen</label>
+                            <textarea name="synopsis" id="synopsis" class="form-control" rows="3"></textarea>
+                        </div>
+
+                        <div class="form-group text-center">
+                            <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+                                Añadir película
+                            </button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
         </div>
-    
-        <div class="form-group">
-            <label for="año">Año:</label>
-            <input type="text" class="form-control" id="año" name="año" required>
-        </div>
-    
-        <div class="form-group">
-            <label for="director">Director:</label>
-            <input type="text" class="form-control" id="director" name="director" required>
-        </div>
-    
-        <div class="form-group">
-            <label for="poster">Poster:</label>
-            <input type="text" class="form-control" id="poster" name="poster" required>
-        </div>
-    
-        <div class="form-group">
-            <label for="synopsis">Resumen:</label>
-            <textarea class="form-control" id="synopsis" name="synopsis" rows="3" required></textarea>
-        </div>
-    
-        <button type="submit" class="btn btn-primary">Añadir película</button>
-    </form>
+    </div>
 @endsection
--- Laravel/videoclub/resources/views/catalog/edit.blade.php
+++ Laravel/videoclub/resources/views/catalog/edit.blade.php
@@ -1,33 +1,49 @@
 @extends('layouts.master')
 
 @section('content')
-    <form action="" method="POST">
-        @csrf
-        <div class="form-group">
-            <label for="titulo">Título:</label>
-            <input type="text" class="form-control" id="titulo" name="titulo" required>
-        </div>
+    <div class="row" style="margin-top:40px">
+        <div class="offset-md-3 col-md-6">
+            <div class="card">
+                <div class="card-header text-center">
+                    Modificar pelicula
+                </div>
+                <div class="card-body" style="padding:30px">
+                    <form action="" method="POST">
+                        @method('PUT')
+                        @csrf
+                        <div class="form-group">
+                            <label for="title">Título</label>
+                            <input type="text" name="title" id="title" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="año">Año</label>
+                            <input type="text" class="form-control" id="año" name="año" required>
+                        </div>
 
-        <div class="form-group">
-            <label for="año">Año:</label>
-            <input type="text" class="form-control" id="año" name="año" required>
-        </div>
+                        <div class="form-group">
+                            <label for="director">Director</label>
+                            <input type="text" class="form-control" id="director" name="director" required>
+                        </div>
 
-        <div class="form-group">
-            <label for="director">Director:</label>
-            <input type="text" class="form-control" id="director" name="director" required>
-        </div>
+                        <div class="form-group">
+                            <label for="poster">Poster</label>
+                            <input type="text" class="form-control" id="poster" name="poster" required>
+                        </div>
 
-        <div class="form-group">
-            <label for="poster">Poster:</label>
-            <input type="text" class="form-control" id="poster" name="poster" required>
-        </div>
+                        <div class="form-group">
+                            <label for="synopsis">Resumen</label>
+                            <textarea name="synopsis" id="synopsis" class="form-control" rows="3"></textarea>
+                        </div>
 
-        <div class="form-group">
-            <label for="synopsis">Resumen:</label>
-            <textarea class="form-control" id="synopsis" name="synopsis" rows="3" required></textarea>
-        </div>
+                        <div class="form-group text-center">
+                            <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+                                Modificar película
+                            </button>
+                        </div>
+                    </form>
 
-        <button type="submit" class="btn btn-primary">Modificar pelicula</button>
-    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
